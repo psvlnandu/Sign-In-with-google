5bf51343abb6a1981fe0a27bb7de05dc8f53ba24
psvlnandu <psvlnandu@gmail.com>
2022-09-17 23:13:18 +0530

    comit-2

---
+++ app/src/main/java/com/example/loginapp/SecondActivity.java
@@ -0,0 +1,62 @@
+package com.example.loginapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+public class SecondActivity extends AppCompatActivity {
+    GoogleSignInOptions gso;
+    GoogleSignInClient gsc;
+    TextView name,email;
+    Button Signoutbtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        name=findViewById(R.id.name);
+        email=findViewById(R.id.email);
+        Signoutbtn=findViewById(R.id.signoutBtn);
+
+        gso=new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        gsc= GoogleSignIn.getClient(this,gso);
+
+        GoogleSignInAccount acc=GoogleSignIn.getLastSignedInAccount(this);
+        if(acc!=null){
+            String pname=acc.getDisplayName();
+            String pemail=acc.getEmail();
+            // String photo=acc.getPhotoUrl();
+            name.setText(pname);
+            email.setText(pemail);
+
+        }
+        Signoutbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signOut();
+            }
+        });
+    }
+    void signOut(){
+        gsc.signOut().addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                finish();
+                startActivity(new Intent(SecondActivity.this,MainActivity.class));
+            }
+        });
+    }
+}
